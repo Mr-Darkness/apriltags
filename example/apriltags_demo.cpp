438786102e1b0243f8da5d6b66dbb713182fc2a6
TianYe <601329141@qq.com>
2016-06-23 15:19:12 +0800

    simply main

--- example/apriltags_demo.cpp
+++ example/apriltags_demo.cpp
@@ -12,8 +12,6 @@
  * option to send tag detections via a serial port, for example when
  * running on a Raspberry Pi that is connected to an Arduino.
  */
- 
-using namespace std;
 
 #include <iostream>
 #include <cstring>
@@ -21,32 +19,7 @@ using namespace std;
 #include <list>
 #include <sys/time.h>
 
-const string usage = "\n"
-  "Usage:\n"
-  "  apriltags_demo [OPTION...] [IMG1 [IMG2...]]\n"
-  "\n"
-  "Options:\n"
-  "  -h  -?          Show help options\n"
-  "  -a              Arduino (send tag ids over serial port)\n"
-  "  -d              Disable graphics\n"
-  "  -t              Timing of tag extraction\n"
-  "  -C <bbxhh>      Tag family (default 36h11)\n"
-  "  -D <id>         Video device ID (if multiple cameras present)\n"
-  "  -F <fx>         Focal length in pixels\n"
-  "  -W <width>      Image width (default 640, availability depends on camera)\n"
-  "  -H <height>     Image height (default 480, availability depends on camera)\n"
-  "  -S <size>       Tag size (square black frame) in meters\n"
-  "  -E <exposure>   Manually set camera exposure (default auto; range 0-10000)\n"
-  "  -G <gain>       Manually set camera gain (default auto; range 0-255)\n"
-  "  -B <brightness> Manually set the camera brightness (default 128; range 0-255)\n"
-  "\n";
-
-const string intro = "\n"
-    "April tags test code\n"
-    "(C) 2012-2014 Massachusetts Institute of Technology\n"
-    "Michael Kaess\n"
-    "\n";
-
+using namespace std;
 
 #ifndef __APPLE__
 #define EXPOSURE_CONTROL // only works in Linux
@@ -77,12 +50,7 @@ const string intro = "\n"
 extern int optind;
 extern char *optarg;
 
-// For Arduino: locally defined serial port access class
-#include "Serial.h"
-
-
 const char* windowName = "apriltags_demo";
-
 
 // utility function to provide current system time (used below in
 // determining frame rate at which images are being processed)
@@ -91,7 +59,6 @@ double tic() {
   gettimeofday(&t, NULL);
   return ((double)t.tv_sec + ((double)t.tv_usec)/1000000.);
 }
-
 
 #include <cmath>
 
@@ -123,8 +90,9 @@ void wRo_to_euler(const Eigen::Matrix3d& wRo, double& yaw, double& pitch, double
     roll  = standardRad(atan2(wRo(0,2)*s - wRo(1,2)*c, -wRo(0,1)*s + wRo(1,1)*c));
 }
 
-vector<Eigen::Matrix4d> Hi; //relative position of tag_i in coordonate tag_0
 
+vector<Eigen::Matrix4d> Hi; //relative position of tag_i in coordonate tag_0
+//initialize the relative position of each tag_i in coordonate tag_0
 void init_Hi()
 {
     Eigen::Matrix4d H00;
@@ -171,8 +139,6 @@ class Demo {
   int m_gain;
   int m_brightness;
 
-  Serial m_serial;
-
   double camera_position_x;
   double camera_position_y;
   double camera_position_z;
@@ -197,11 +163,6 @@ public:
     m_fy(527.686373),
     m_px(334.880067),
     m_py(235.939132),
-    //m_tagSize(0.166),
-    //m_fx(600),
-    //m_fy(600),
-    //m_px(m_width/2),
-    //m_py(m_height/2),
     
     camera_position_x(0.0),
     camera_position_y(0.0),
@@ -232,87 +193,6 @@ public:
     }
   }
 
-  // parse command line options to change default behavior
-  void parseOptions(int argc, char* argv[]) {
-    int c;
-    while ((c = getopt(argc, argv, ":h?adtC:F:H:S:W:E:G:B:D:")) != -1) {
-      // Each option character has to be in the string in getopt();
-      // the first colon changes the error character from '?' to ':';
-      // a colon after an option means that there is an extra
-      // parameter to this option; 'W' is a reserved character
-      switch (c) {
-      case 'h':
-      case '?':
-        cout << intro;
-        cout << usage;
-        exit(0);
-        break;
-      case 'a':
-        m_arduino = true;
-        break;
-      case 'd':
-        m_draw = false;
-        break;
-      case 't':
-        m_timing = true;
-        break;
-      case 'C':
-        setTagCodes(optarg);
-        break;
-      case 'F':
-        m_fx = atof(optarg);
-        m_fy = m_fx;
-        break;
-      case 'H':
-        m_height = atoi(optarg);
-        m_py = m_height/2;
-         break;
-      case 'S':
-        m_tagSize = atof(optarg);
-        break;
-      case 'W':
-        m_width = atoi(optarg);
-        m_px = m_width/2;
-        break;
-      case 'E':
-#ifndef EXPOSURE_CONTROL
-        cout << "Error: Exposure option (-E) not available" << endl;
-        exit(1);
-#endif
-        m_exposure = atoi(optarg);
-        break;
-      case 'G':
-#ifndef EXPOSURE_CONTROL
-        cout << "Error: Gain option (-G) not available" << endl;
-        exit(1);
-#endif
-        m_gain = atoi(optarg);
-        break;
-      case 'B':
-#ifndef EXPOSURE_CONTROL
-        cout << "Error: Brightness option (-B) not available" << endl;
-        exit(1);
-#endif
-        m_brightness = atoi(optarg);
-        break;
-      case 'D':
-        m_deviceId = atoi(optarg);
-        break;
-      case ':': // unknown option, from getopt
-        cout << intro;
-        cout << usage;
-        exit(1);
-        break;
-      }
-    }
-
-    if (argc > optind) {
-      for (int i=0; i<argc-optind; i++) {
-        m_imgNames.push_back(argv[optind+i]);
-      }
-    }
-  }
-
   void setup() {
     m_tagDetector = new AprilTags::TagDetector(m_tagCodes);
 
@@ -321,10 +201,6 @@ public:
       cv::namedWindow(windowName, 1);
     }
 
-    // optional: prepare serial port for communication with Arduino
-    if (m_arduino) {
-      m_serial.open("/dev/ttyACM0");
-    }
   }
 
   void setupVideo() {
@@ -377,49 +253,6 @@ public:
 
   }
 
-  /*
-  void print_detection(AprilTags::TagDetection& detection) const {
-    cout << "  Id: " << detection.id
-         << " (Hamming: " << detection.hammingDistance << ")";
-
-    // recovering the relative pose of a tag:
-
-    // NOTE: for this to be accurate, it is necessary to use the
-    // actual camera parameters here as well as the actual tag size
-    // (m_fx, m_fy, m_px, m_py, m_tagSize)
-
-    Eigen::Vector3d translation;
-    Eigen::Matrix3d rotation;
-    detection.getRelativeTranslationRotation(m_tagSize, m_fx, m_fy, m_px, m_py,
-                                             translation, rotation);
-
-    Eigen::Matrix3d F;
-    F <<
-      1, 0,  0,
-      0,  -1,  0,
-      0,  0,  1;
-    Eigen::Matrix3d fixed_rot = F*rotation;
-    double yaw, pitch, roll;
-    wRo_to_euler(fixed_rot, yaw, pitch, roll);
-
-    cout << "  distance=" << translation.norm()
-         << "m, x=" << translation(0)
-         << ", y=" << translation(1)
-         << ", z=" << translation(2)
-         << ", yaw=" << yaw
-         << ", pitch=" << pitch
-         << ", roll=" << roll
-         << endl;
-
-    //geometry_msgs::Point point;
-
-
-    // Also note that for SLAM/multi-view application it is better to
-    // use reprojection error of corner points, because the noise in
-    // this relative pose is very non-Gaussian; see iSAM source code
-    // for suitable factors.
-  }
-*/
   void print_relation(vector<AprilTags::TagDetection> detections)
   {
       Eigen::Matrix4d T0cam;
@@ -485,11 +318,7 @@ public:
 
     // print out each detection
     cout << detections.size() << " tags detected:" << endl;
-/*
-    for (int i=0; i<detections.size(); i++) {
-      print_detection(detections[i]);
-    }
-*/
+
     if(detections.size() > 0)
         print_relation(detections);
 
@@ -500,42 +329,6 @@ public:
         detections[i].draw(image);
       }
       imshow(windowName, image); // OpenCV call
-    }
-
-    // optionally send tag information to serial port (e.g. to Arduino)
-    /*
-    if (m_arduino) {
-      if (detections.size() > 0) {
-        // only the first detected tag is sent out for now
-        Eigen::Vector3d translation;
-        Eigen::Matrix3d rotation;
-        detections[0].getRelativeTranslationRotation(m_tagSize, m_fx, m_fy, m_px, m_py,
-                                                     translation, rotation);
-        m_serial.print(detections[0].id);
-        m_serial.print(",");
-        m_serial.print(translation(0));
-        m_serial.print(",");
-        m_serial.print(translation(1));
-        m_serial.print(",");
-        m_serial.print(translation(2));
-        m_serial.print("\n");
-      } else {
-        // no tag detected: tag ID = -1
-        m_serial.print("-1,0.0,0.0,0.0\n");
-      }
-    }
-    */
-  }
-
-  // Load and process a single image
-  void loadImages() {
-    cv::Mat image;
-    cv::Mat image_gray;
-
-    for (list<string>::iterator it=m_imgNames.begin(); it!=m_imgNames.end(); it++) {
-      image = cv::imread(*it); // load image with opencv
-      processImage(image, image_gray);
-      while (cv::waitKey(100) == -1) {}
     }
   }
 
@@ -567,7 +360,6 @@ public:
         cout << "  " << 10./(t-last_t) << " fps" << endl;
         last_t = t;
       }
-
       // exit if any key is pressed
       if (cv::waitKey(1) >= 0) break;
     }
@@ -575,34 +367,19 @@ public:
 
 }; // Demo
 
-
 // here is were everything begins
 int main(int argc, char* argv[]) {
   Demo demo;
-
   init_Hi();
-
-  // process command line options
-  demo.parseOptions(argc, argv);
 
   demo.setup();
 
   if (demo.isVideo()) {
     cout << "Processing video" << endl;
-
     // setup image source, window for drawing, serial port...
     demo.setupVideo();
-
     // the actual processing loop where tags are detected and visualized
     demo.loop();
-
-  } else {
-    cout << "Processing image" << endl;
-
-    // process single image
-    demo.loadImages();
-
-  }
-
+  } 
   return 0;
 }
